abf10a40f4e11563330df7e976485b1c3a982af7
Lulily13 <bloodyakuma13@yahoo.com>
2022-02-05 13:24:25 +0100

    Created Calculator constructor, added js file to gulp and html

---
+++ js/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM fully loaded and parsed!");
+
+    function Calculator(form, summary) {
+        this.prices = {
+            products: 0.5,
+            orders: 0.25,
+            package: {
+                basic: 0,
+                professional: 25,
+                premium: 60
+            },
+            accounting: 35,
+            terminal: 5
+        };
+    }
+
+
+});
